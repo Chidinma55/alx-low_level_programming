454b9f86ab0fec6667ac3c89a5cdeab3d3e4a751
Bene Elohim Hub <comfort2210@gmail.com>
2023-08-10 08:41:31 -0700

    array_range

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ char *_memset(char *s, char b, unsigned int n)
 		s[i] = b;
 	}
 
-	return(s);
+	return (s);
 /**
  * *_calloc - allocates memory for an array
  * @nmemb: number of elementsof elements in the array
